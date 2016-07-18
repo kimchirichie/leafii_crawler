42d50f68d0bfa2e678edb78edbdcc08d46506f2a
Jisung Im <j4im@uwaterloo.ca>
2016-07-18 18:06:57 -0400

    Update testDB.py

--- testDB.py
+++ testDB.py
@@ -11,17 +11,33 @@ db = client.meteor
 data = ["your data here"]
 
 key_dict = db.keywords
+
 for i in range(len(data)):
 	data_temp = data[i]
 	url_temp = (data_temp.get("profile").get("url"))
 	id_temp = (data_temp.get("_id"))
 
 	tags = get_html(url_temp)
+	tagsPDF = get_pdf(url_temp)
 
-	key_db = {"keywords": tags, "url": url_temp, "user_id": id_temp}
+	for k in range(len(tags)):
 
-	key_dict_id = key_dict.insert_one(key_db).inserted_id
-	#print (key_dict_id)
+		key_db = {"keywords": tags[k], "url": url_temp, "user_id": id_temp, "pdf ": "False"}
+
+		#print key_db
+
+		key_dict_id = key_dict.insert_one(key_db).inserted_id
+		#print (key_dict_id)
+
+	for j in range(len(tagsPDF)):
+
+		key_db = {"keywords": tagsPDF[j], "url": url_temp, "user_id": id_temp, "pdf ": "True"}
+
+		#print key_db
+
+		key_dict_id = key_dict.insert_one(key_db).inserted_id
+		#print (key_dict_id)
+
 
 
 
