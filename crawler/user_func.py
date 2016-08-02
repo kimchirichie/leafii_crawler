ccf915df6c656133e6059b340c13119af48a2204
abhi-m97 <abhishek.mukherjee742@gmail.com>
2016-08-02 11:43:00 -0400

    Fixed errors

--- crawler/user_func.py
+++ crawler/user_func.py
@@ -10,77 +10,6 @@ class bcolors:
 	ENDC = '\033[0m'
 	OKBLUE = '\033[94m'
 
-def parse(data):
-	for i in range(len(data)):
-		data_temp = data[i]
-
-		# obtain url and id from user data
-
-		url_temp = (data_temp.get("profile").get("url"))
-		print bcolors.OKGREEN + ("Running through..... " + url_temp) + bcolors.ENDC
-		id_temp = (data_temp.get("_id"))
-
-		# there will be 2 types of html,
-		# from website, and from pdf
-
-		tags_temp = get_html(url_temp) # this is keywords from the html
-		# print tags_temp
-		tagsPDF_temp = get_pdf(url_temp) # this is keywords from the pdf
-		# print tagsPDF_temp
-
-		seen = set()
-		tags = []
-		tagsPDF = []
-
-		for item in tags_temp:
-			if item not in seen:
-				seen.add(item)
-				tags.append(item)
-				# print "seen:", seen
-				# print "tags:", tags, "\n"
-
-		for item in tagsPDF_temp:
-			if item not in seen:
-				seen.add(item)
-				tagsPDF.append(item)
-				# print "seen:", seen
-				# print "tags:", tagsPDF, "\n"
-
-		# update the mongoDB with html keywords, with another id generated
-		seen2 = set()
-		for k in range(len(tags)):
-			#print ("Keywords Website:" + url_temp)
-			seperateTags = tags[k].split(" ")
-			for l in range(len(seperateTags)):
-				if seperateTags[l] not in seen2:
-					seen2.add(seperateTags[l])
-					key_db = {"keyword": seperateTags[l].lower(), "url": url_temp, "user_id": id_temp, "type": "web"}
-					print key_db
-					# print seen2
-
-					key_dict_id = key_dict.insert_one(key_db).inserted_id
-
-		# update the mongoDB with pdf keywords, with another id generated
-
-		for j in range(len(tagsPDF)):
-			#print ("Keywords PDF:" + url_temp)
-			seperateTagspdf = tagsPDF[j].split(" ")
-			for h in range(len(seperateTagspdf)):
-				if seperateTagspdf[h] not in seen2:
-					seen2.add(seperateTagspdf[h])
-					key_db = {"keyword": seperateTagspdf[h].lower(), "url": url_temp, "user_id": id_temp, "type": "pdf"}
-					print key_db
-
-					key_dict_id = key_dict.insert_one(key_db).inserted_id
-
-		print bcolors.OKBLUE + "--------------------------------------------" + bcolors.ENDC + '\n'
-
-
-	print bcolors.OKGREEN + ("Took %s seconds total" % (time.time() - start_time)) + bcolors.ENDC
-	print bcolors.OKGREEN + "Went through " + str(len(data)) + " web pages" + bcolors.ENDC
-	print bcolors.OKGREEN + "Generated " + str(db.keywords_coll.count()) + " keywords" + bcolors.ENDC
-
-#working
 def find_user_by_id(user_id):
 	#returns a user's info with their id
 	try:
@@ -103,7 +32,7 @@ def find_user_by_id(user_id):
 	except Exception, e:
 		print e
 		return False
-#working
+
 def find_user_by_email(email):
 	#returns a user's info with their email
 	try:
@@ -120,11 +49,12 @@ def find_user_by_email(email):
 			if temp_email[0].get("address") == email:
 				print "Found"
 				print data[i]
+				print "User ID: " + data[i].get("_id")
 				return data[i]	
 	except Exception, e:
 		print e
 		return False
-#working
+
 def find_user_by_name(name):
 	#returns a list of all user's who have the input name as their first or last name
 	try:
@@ -148,7 +78,7 @@ def find_user_by_name(name):
 				#print temp_profile.get("firstName")
 				if name in temp_profile.get("firstName") or name in temp_profile.get("lastName") :
 					#print temp_profile
-					user_list = user_list + [temp_profile]
+					user_list = user_list + [data[i]]
 
 		elif len(temp_name) == 2:
 			#searches for the full name with exact match
@@ -157,7 +87,7 @@ def find_user_by_name(name):
 			for i in range(len(data)):
 				temp_profile = data[i].get("profile")
 				if temp_firstName in temp_profile.get("firstName") and temp_lastName in temp_profile.get("lastName"):  
-					user_list = user_list + [temp_profile]
+					user_list = user_list + [data[i]]
 
 		elif len(temp_name) > 2:
 			#if more than three words are in name search, checks each word against first and last name 
@@ -165,16 +95,18 @@ def find_user_by_name(name):
 				for c in range(len(temp_name)):
 					temp_profile = data[i].get("profile")
 					if temp_name[c] in temp_profile.get("firstName") or temp_name[c] in temp_profile.get("lastName"):
-						user_list = user_list + [temp_profile]
+						user_list = user_list + [data[i]]
+
 
 		for i in range(len(user_list)):
 			print user_list[i]
+			print ""
 		return user_list
 
 	except Exception, e:
 		print e
 		return False
-#keywords is for total users
+
 def parse_user_site(user_id):
 	#parses through a user's site 
 	try:
@@ -182,7 +114,7 @@ def parse_user_site(user_id):
 		client = MongoClient('mongodb://127.0.0.1:3001/meteor')
 		db = client.meteor
 		data = []
-
+		key_dict = db.keywords_coll
 		# data is user data from user collection.
 		# we will be uploading our keywords to
 		# keywords collection.
@@ -222,7 +154,7 @@ def parse_user_site(user_id):
 				tagsPDF.append(item)
 				# print "seen:", seen
 				# print "tags:", tagsPDF, "\n"
-
+		key_count = 0
 		# update the mongoDB with html keywords, with another id generated
 		seen2 = set()
 		for k in range(len(tags)):
@@ -232,6 +164,7 @@ def parse_user_site(user_id):
 				if seperateTags[l] not in seen2:
 					seen2.add(seperateTags[l])
 					key_db = {"keyword": seperateTags[l].lower(), "url": url_temp, "user_id": id_temp, "type": "web"}
+					key_count += 1
 					print key_db
 					# print seen2
 
@@ -246,6 +179,7 @@ def parse_user_site(user_id):
 				if seperateTagspdf[h] not in seen2:
 					seen2.add(seperateTagspdf[h])
 					key_db = {"keyword": seperateTagspdf[h].lower(), "url": url_temp, "user_id": id_temp, "type": "pdf"}
+					key_count += 1
 					print key_db
 
 					key_dict_id = key_dict.insert_one(key_db).inserted_id
@@ -255,7 +189,7 @@ def parse_user_site(user_id):
 
 		print bcolors.OKGREEN + ("Took %s seconds total" % (time.time() - start_time)) + bcolors.ENDC
 		print bcolors.OKGREEN + "Went through one web page" + bcolors.ENDC
-		print bcolors.OKGREEN + "Generated " + str(db.keywords_coll.count()) + " keywords" + bcolors.ENDC
+		print bcolors.OKGREEN + "Generated " + str(key_count) + " keywords" + bcolors.ENDC
 
 	except Exception, e:
 		print e
@@ -264,7 +198,12 @@ def parse_user_site(user_id):
 def parse_all_users():
 	#parses through all the users' sites
 	try:
-		connect()
+		
+		start_time = time.time()
+		client = MongoClient('mongodb://127.0.0.1:3001/meteor')
+		db = client.meteor
+		data = []
+		key_dict = db.keywords_coll
 
 		# data is user data from user collection.
 		# we will be uploading our keywords to
@@ -272,64 +211,139 @@ def parse_all_users():
 		for i in db.users.find():
 			data = data + [i]
 
-		# Now, data[0] is data for 1st user.
+		for i in range(len(data)):
+		    data_temp = data[i]
 
-		# our collection is called keywords
-		key_dict = db.keywords_coll
+		    # obtain url and id from user data
 
-		parse()
+		    url_temp = (data_temp.get("profile").get("url"))
+		    print bcolors.OKGREEN + ("Running through..... " + url_temp) + bcolors.ENDC
+		    id_temp = (data_temp.get("_id"))
+
+		    # there will be 2 types of html,
+		    # from website, and from pdf
+
+		    tags_temp = get_html(url_temp) # this is keywords from the html
+		    # print tags_temp
+		    tagsPDF_temp = get_pdf(url_temp) # this is keywords from the pdf
+		    # print tagsPDF_temp
+
+		    seen = set()
+		    tags = []
+		    tagsPDF = []
+
+		    for item in tags_temp:
+		        if item not in seen:
+		            seen.add(item)
+		            tags.append(item)
+		            # print "seen:", seen
+		            # print "tags:", tags, "\n"
+
+		    for item in tagsPDF_temp:
+		        if item not in seen:
+		            seen.add(item)
+		            tagsPDF.append(item)
+		            # print "seen:", seen
+		            # print "tags:", tagsPDF, "\n"
+
+		    # update the mongoDB with html keywords, with another id generated
+		    seen2 = set()
+		    for k in range(len(tags)):
+		        #print ("Keywords Website:" + url_temp)
+		        seperateTags = tags[k].split(" ")
+		        for l in range(len(seperateTags)):
+		            if seperateTags[l] not in seen2:
+		                seen2.add(seperateTags[l])
+		                key_db = {"keyword": seperateTags[l].lower(), "url": url_temp, "user_id": id_temp, "type": "web"}
+		                print key_db
+		                # print seen2
+
+		                key_dict_id = key_dict.insert_one(key_db).inserted_id
+
+		    # update the mongoDB with pdf keywords, with another id generated
+
+		    for j in range(len(tagsPDF)):
+		        #print ("Keywords PDF:" + url_temp)
+		        seperateTagspdf = tagsPDF[j].split(" ")
+		        for h in range(len(seperateTagspdf)):
+		            if seperateTagspdf[h] not in seen2:
+		                seen2.add(seperateTagspdf[h])
+		                key_db = {"keyword": seperateTagspdf[h].lower(), "url": url_temp, "user_id": id_temp, "type": "pdf"}
+		                print key_db
+
+		                key_dict_id = key_dict.insert_one(key_db).inserted_id
+
+		    print bcolors.OKBLUE + "--------------------------------------------" + bcolors.ENDC + '\n'
+
+		print bcolors.OKGREEN + ("Took %s seconds total" % (time.time() - start_time)) + bcolors.ENDC
+		print bcolors.OKGREEN + "Went through " + str(len(data)) + " web pages" + bcolors.ENDC
+		print bcolors.OKGREEN + "Generated " + str(db.keywords_coll.count()) + " keywords" + bcolors.ENDC
+		
 		return True
 
-	except:
-		print "ERROR: Can't parse all users"
+	except Exception, e:
+		print e
 		return False
 
 def delete_user_keywords(user_id):
 	#deletes all of a single user's keywords
 	try:
-		connect()
+		start_time = time.time()
+		client = MongoClient('mongodb://127.0.0.1:3001/meteor')
+		db = client.meteor
+		data = []
+
+		for i in db.users.find():
+			data = data + [i]
+
 		key_dict = db.keywords_coll
 
 		# adds all of a user's keywords to user_keywords to return the old list of user's keywords
 		user_keywords = []
-		for i in range(len(key_dict)):
-			if user_id in key_dict[i]:
-				user_keywords = user_keywords + [key_dict[i]]
+		
+		#for i in range(len(key_dict)):
+		#	if user_id in key_dict[i]:
+		#		user_keywords = user_keywords + [key_dict[i]]
 
 		#deletes existing user data
 		result = key_dict.delete_many({"user_id": user_id})
-		return user_keywords
-	except:
-		print "ERROR: Can't delete keywords from user"
+		#print user_keywords
+		print result
+
+	except Exception, e:
+		print e
+		return False
 
 def delete_all_keywords():
 	#deletes all keywords of all users
 	try:
-		connect()
+		start_time = time.time()
+		client = MongoClient('mongodb://127.0.0.1:3001/meteor')
+		db = client.meteor
+		data = []
+
+		for i in db.users.find():
+			data = data + [i]
+
 		key_dict = db.keywords_coll
 		#deletes existing data
 		result = key_dict.delete_many({})
 		print "Entries Deleted"
 		return True
 
-	except:
-		print "ERROR: Can't delete keyword from all users"
+	except Exception, e:
+		print e
 		return False
 
 def re_parse_all():
 	#deletes existing keywords and reparses through them all
 	try:
-		connect()
+		
 		#wipes existing keywords
 		delete_all_keywords()
 		#parse all users
 		parse_all_users()
 
-	except:
-		print("ERROR: Reparsing Failed")
+	except Exception, e:
+		print e
 		return False
-
-
-
-
-parse_user_site("uz1HIO5OpdH540GuG")
